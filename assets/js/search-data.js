f42bf3ac7a94359c3c8b5b76173e0e7873f6c52c
ibuhnila <73898873+ibuhnila@users.noreply.github.com>
2026-04-06 05:44:53 +0000

    Deploying to gh-pages from @ ibuhnila/ibuhnila.github.io@20846ba9160b05dbbac423ee2c4c448a83f6dff0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "Ioana Buhnila&#39;s CV",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
