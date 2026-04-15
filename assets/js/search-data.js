2e93235e02fd5ca6501746351c68971423d84aae
ibuhnila <73898873+ibuhnila@users.noreply.github.com>
2026-04-15 13:57:20 +0000

    Deploying to gh-pages from @ ibuhnila/ibuhnila.github.io@26cc22d8bae8b94ebd00e5e7091652ad54651e8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research-projects",
+          title: "Research Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
